3fa8a3917c2d240984c94ded18217eb98dd0c18f
Partarum <email@partarum.de>
2025-09-14 07:54:23 +0200

    Solution with fix v.0.3

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -32,5 +32,5 @@ def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    assert split_integer(1, 18) == [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
-    assert split_integer(37, 38) == [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    assert split_integer(1, 3) == [0, 0, 1]
+    assert split_integer(2, 5) == [0, 0, 0, 1, 1]
